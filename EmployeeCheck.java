f8f097acdd945ef0c863b0389de10d7ff9965889
PeKkA01 <163143405+PeKkA01@users.noreply.github.com>
2026-01-09 01:51:44 +0530

    Add files via upload

--- EmployeeCheck.java
+++ EmployeeCheck.java
@@ -1,38 +1,45 @@
 import java.util.*;
-class EmployeeW{
-    int working_hour = 8;
-    int wageper_hour = 20;
-    int parttime_hour = 4;
-    int working_days = 20;
-    double salary;
-    void calculate_salary(){
-        salary = working_days*working_hour*wageper_hour;
-        System.out.println(salary);
-    }
-}
-public class EmployeeCheck {
+public class EmployeeCheck{
+    public static  int is_part_time=1;
+    public static   int is_full_time=2;
+    public static  int emp_rate_per_hour=20;
+    public static  int total_num_Work_day=2;
+    public static  int max_hrs=10;
     public static void main(String[] args) {
-        Random r = new Random();
-        EmployeeW e = new EmployeeW();
-        int is_fulltime = 1;
-        int is_parttime = 2;
-        int i = r.nextInt(1000)%3;
-        switch (i){
-            case 1: {
-                if(i==1){
-                    System.out.println("Full_Time Employee");
+        int emp_hrs=0;
+        int emp_wage=0;
+        int total_wage=0;
+        int total_working_days=0;
+        int total_emp_hours=0;
+        Random r=new Random();
+        while(total_emp_hours <= max_hrs && total_working_days < total_num_Work_day){
+            total_working_days++;
+            int emp_check=r.nextInt(1000)%3;
+            switch(emp_check){
+                case 1:{
+                    if(emp_check==is_full_time){
+                        emp_hrs=8;
+                        break;
+                    }
+                }
+                case 2:{
+                    if(emp_check==is_part_time){
+                        emp_hrs=4;
+                        break;
+                    }
+                }
+                default:{
+                    emp_hrs=0;
                     break;
                 }
-            }
-            case 2: {
-                System.out.println("Part_Time Employee");
-                break;
-            }
-            default:{
-                System.out.println("Absent");
-            }
-        }
-        e.calculate_salary();
 
+            }
+            emp_wage=emp_hrs*emp_rate_per_hour;
+            total_wage += emp_wage;
+            System.out.println("Emp Wage: "+emp_wage);
+            total_emp_hours += emp_hrs;
+            System.out.println("Total Wage: "+total_wage);
+        }
     }
+
 }
