72b205363dc681d29eb4809b50238b1302a20919
PeKkA01 <163143405+PeKkA01@users.noreply.github.com>
2026-01-11 23:54:10 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pattern{
+    public static void main(String[] args) {
+//        System.out.println("print number from 1 to n");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        System.out.println("enter the value of n");
+//        for (int i = 1; i <=n ; i++) {
+//            System.out.println(i);
+//        }
+
+        System.out.println("enter number from n to 1");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("enter value of n ");
+        for (int i = 1; i >=n ; i--) {
+            System.out.println(i);
+        }
+
+
+
+
+
+        }
+    }
